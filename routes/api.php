81d4bbf28c5016f76c0104f4b09797321ed113b9
Rizki Alfian <rizkialfiansss@yahoo.com>
2019-06-09 17:35:06 +0700

    Tutorial done, source Mulia

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,25 @@
 <?php
 
-//use App\Item; //membuat model class item di namespace app
+use App\Item; //membuat model class item di namespace app
 use Illuminate\Http\Request;
 
 Route::get('items', function (){
-    return [
-        'message' => 'ini adalah browser',
-    ];
+    return Item::paginate(5); //dibagi setiap 5 potongan dan bisa dibuat all 
+});
+
+Route::get('items/{item}', function ($id){
+    return Item::find($id);
+});
+//bisa yg di atas atau yg di bawah kalau ingin mencari berdasarkan id
+// Route::get('items/{item}', function (Item $item){
+//     return $item;
+// });
+
+Route::post('items', function (Item $item){
+    return Item::create(request()->all());
+});
+
+Route::delete('items/{item}', function (Item $item){
+    $item->delete();
+    return  'Success';
 });
